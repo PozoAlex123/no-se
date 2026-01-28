e73c63a0c99063c481cdc94b4e0d4c538894027f
Ale <alejandropozo2031@gmail.com>
2026-01-28 09:44:37 -0400

    added multiplication and division

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 print("calculator program")
 
 try:
-    eleccion = input("choose 1 of the following options\n1. addition(+)\n2. substraction(-)\n")
+    eleccion = input("choose 1 of the following options\n1. addition(+)\n2. substraction(-)\n3. multiplication(*)\n4. division(/)")
     num1 = int(input("give me a number: "))
     num2 = int(input("give me another number: "))
 
@@ -10,6 +10,13 @@ try:
         print(f"The result is: {num1 + num2}")
     elif eleccion == '2':
         print(f"The result is: {num1 - num2}")
+    elif eleccion == '3':
+        print(f"The result is :{num1 * num2}")
+    elif eleccion == '4':
+        if num2 == '0':
+            print(f"You can't divide by zero")    
+        else:
+            print(f"The result is: {num1 / num2}")
     else:
         print("you can only choose the options above")
 
